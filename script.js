6a38b917e635e905a15d9cbb3db14000780eedbf
Ulfiya Nurul Hidayati <62195213+ulfiyanurulhidayati@users.noreply.github.com>
2020-04-21 16:59:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+canvas.width= canvas.scrollWidth;
+canvas.height= canvas.scrollHeight;
+context.height= context.scrollWidth;
+context.height= context.scrollHeight;
 
+//gelas tertutup
+context.beginPath();
+context.moveTo(170,80);
+context.rect(188,150,130,200);
+context.fillStyle = 'salmon';
+context.fill();
+
+context.beginPath();
+context.moveTo(170,80);
+context.rect(188,140,130,30);
+context.fillStyle = 'black';
+context.fill();
+
+context.beginPath();
+context.moveTo(170,80);
+context.rect(245,120,20,20);
+context.fillStyle = 'salmon';
+context.fill();
