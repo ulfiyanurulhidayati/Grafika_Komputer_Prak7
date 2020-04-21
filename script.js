f52f76d5b90f18834bb21217abc99f102ebc4c24
Karina Hadi <62169958+KarinaHadi@users.noreply.github.com>
2020-04-21 17:00:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,23 @@ context.moveTo(170,80);
 context.rect(245,120,20,20);
 context.fillStyle = 'salmon';
 context.fill();
+
+//Gambar Gelas Terbuka dan Tertutup
+context.beginPath();
+context.moveTo(170,80);
+context.rect(500,150,130,200);
+context.fillStyle = 'salmon';
+context.fill();
+
+context.beginPath();
+context.moveTo(170,80);
+context.rotate(75 * Math.PI/360);
+context.rect(462,-293,130,30);
+context.fillStyle = 'black';
+context.fill();
+
+context.beginPath();
+context.moveTo(170,80);
+context.rect(520,-305,20,20);
+context.fillStyle = 'salmon';
+context.fill();
